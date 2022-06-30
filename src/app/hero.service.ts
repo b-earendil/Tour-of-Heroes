76239ed55a9877b63ffa41255f1e5069b7e4706c
Ben Adams <BKAdams@jhacorp.com>
2022-06-30 13:07:53 -0500

    Refactor current HeroService to use HttpClient
    Update getHeroes() method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,10 +15,10 @@ export class HeroService {
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
