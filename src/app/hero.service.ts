d5356d24bbf5f02ee33f4c7e4dcf5769546bacba
Ben Adams <BKAdams@jhacorp.com>
2022-06-30 14:27:58 -0500

    Add Error Handling to hero.service.ts
    Refactor HeroService.getHeroes() to catch errors
    - To catch, pipe the observable result from http.get() through an RxJS catcherror() operator
    - Import catchError symbol, along with some other operators we'll soon need
    - Extend observable result with the pipe() method and give it a catchError() operator
    - The catchError() operator intercepts an Observable that failed and passes the error to the error handling function
    - The handleError() method repors the error and then returns an innocuous result so the app can continue working
    - Instead of handling the error directly, the handleError() method returns an error handler function to catchError() that it has configured with both the name of the failing operation and a safe return value
    
    Note: Because each service method returns a different kind of Observable result, handleError() takes a type parameter so it can return the safe value as the type the app expects

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -19,6 +20,9 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
@@ -32,5 +36,26 @@ export class HeroService {
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
